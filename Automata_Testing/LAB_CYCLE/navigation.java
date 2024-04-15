234cc729b1210b8f8b1536c8e5c602dd5808f446
nvn <102863982+nvn18@users.noreply.github.com>
2024-04-15 19:04:50 +0530

    Update navigation.java

--- Automata_Testing/LAB_CYCLE/navigation.java
+++ Automata_Testing/LAB_CYCLE/navigation.java
@@ -8,14 +8,16 @@ public class navigation {
 	public static void main(String[] args) {
 		
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
-		driver.navigate().to("https://www.facebook.com");
-		driver.navigate().back();
-		driver.navigate().forward();
-		driver.navigate().refresh();
-		String s = driver.getTitle();
+		driver.get("https://www.google.com"); // picks to google website
+		 driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000)); // delays for 5seconds
+		driver.navigate().to("https://www.facebook.com");// navigates to the facebook website
+		
+		driver.navigate().back(); // navigates back to google webiste
+		driver.navigate().forward(); // navigates forward to facebook website
+		driver.navigate().refresh(); // refreshes the facebook website 
+		String s = driver.getTitle(); // gets the title of current webpage
 		System.out.println("The title is:"+s);
-		String url = driver.getCurrentUrl();
+		String url = driver.getCurrentUrl(); // gets the current url of the webpage
 		System.out.println("The Current URl is:"+url);
 	}
 
