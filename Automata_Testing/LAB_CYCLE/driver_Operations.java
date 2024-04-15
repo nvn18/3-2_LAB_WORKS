c913e93ee4be5a35fde42f42e4bfc4a6f7822791
nvn <102863982+nvn18@users.noreply.github.com>
2024-04-15 19:01:13 +0530

    Update driver_Operations.java

--- Automata_Testing/LAB_CYCLE/driver_Operations.java
+++ Automata_Testing/LAB_CYCLE/driver_Operations.java
@@ -11,9 +11,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class driver_Operations{
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.lambdatest.com/selenium-playground/input-form-demo");
-        String s = driver.getTitle();
+        driver.get("https://www.google.com");  //opens the google webpage
+        String s = driver.getTitle();//gets the title of the webpage
         System.out.println("The title is:"+s);
+        System.out.println("The Class is:"+s.getClass());// prints the current class of the object
+         
+        String current_url = driver.getCurrentUrl();// picks up the current url of the webpage
+        System.out.println("The current Url is:"+current_url);
+        
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));//delays for the 2seconds
+        
+        driver.navigate().to("https://www.lambdatest.com/selenium-playground/input-form-demo");//navigates to lambda testwebsite
         
         WebElement elementById = driver.findElement(By.id("inputEmail4"));
         elementById.sendKeys("neeraj@gmail.com");
@@ -23,23 +31,7 @@ public class driver_Operations{
         elementByName.sendKeys("Neeraj");
         System.out.println(elementByName);
 
-        WebElement elementByClass = driver.findElement(By.className("py-50"));
-        System.out.println(elementByClass);
         
-        WebElement elementByTag = driver.findElement(By.tagName("a"));
-        System.out.println(elementByTag);
-        
-        WebElement elementByLinkText = driver.findElement(By.linkText("Automation Testing Cloud"));
-        System.out.println(elementByLinkText);
-        
-        WebElement elementByPartialLinkText = driver.findElement(By.partialLinkText("Testing Cloud"));
-        System.out.println(elementByPartialLinkText);
-        
-        WebElement elementByXPath = driver.findElement(By.xpath("//*[@id=\"company\"]"));
-        System.out.println(elementByXPath);
-
-        WebElement elementByCssSelector = driver.findElement(By.cssSelector("input#company"));
-        System.out.println(elementByCssSelector);
-        driver.quit();
+        driver.quit();// quits the webpage 
     }
 }
